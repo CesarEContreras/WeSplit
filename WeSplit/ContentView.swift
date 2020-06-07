47e1b5f2243d7c5012d4be51b1232a999c840009
Cesar Contreras <cesar.contrerasm@outlook.com>
2020-06-07 08:42:33 -0400

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Cesar Contreras on 6/7/20.
+//  Copyright © 2020 Cesar Contreras. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
