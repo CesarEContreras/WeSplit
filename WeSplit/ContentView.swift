3ba5e64fb982f2dab02ce011ee727c3f747c2efd
Cesar Contreras <cesar.contrerasm@outlook.com>
2020-06-07 11:13:35 -0400

    Adding a form with Groups and Sections

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,7 +10,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Form {
+            Group {
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+            }
+            
+            Group {
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+            }
+            
+            Section {
+                Text("Hello, World!")
+            }
+            
+            Section {
+                Text("Hello, World!")
+                Text("Hello, World!")
+            }
+        }
     }
 }
 
